527eb04c91125fbef479ff2b3fdbfe1b64918ed8
Carter Brainerd <cbawsome77@gmail.com>
2016-10-27 11:31:24 -0400

    add untested timer for 100 seconds

--- mail0wner.py
+++ mail0wner.py
@@ -1,4 +1,9 @@
 from scapy.all import *
+import time
+
+start_time = time.time()
+
+
 
 #packet callback
 def packet_callback(packet):
@@ -8,8 +13,16 @@ def packet_callback(packet):
             print "[*] Server: %s" % packet[IP].dst
             print "[*] %s" % packet[TCP].payload
 
-#start sniffer
-sniff(filter="tcp port 110 or tcp port 25 or tcp port 143", prn=packet_callback, store=0)
-#port 110 = POP3
-#port 143 = IMAP
-#port 25  = SMTP
+current_time = start_time - time.time()
+total_time = start_time - current_time
+print('[*] Running for 100 seconds...
+while total_time <= 100:
+    
+    #start sniffer
+    sniff(filter="tcp port 110 or tcp port 25 or tcp port 143", prn=packet_callback, store=0)
+    #port 110 = POP3
+    #port 143 = IMAP
+    #port 25  = SMTP
+print("[!] Scanner has finished after 100 seconds.")
+exit(0)
+ 
