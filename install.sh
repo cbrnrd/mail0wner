e3869048aa84874f2b24c7f96701700a3038e38e
thecarterb <cbawsome77@gmail.com>
2016-11-03 16:57:07 -0400

    remove sudo for installing pip

--- install.sh
+++ install.sh
@@ -4,7 +4,9 @@ clear
 echo "If there are any error messages concerning installs, install whatever they tell you to install \nusing sudo apt-get"
 
 echo "Installing pip"
-sudo easy_install pip
+easy_install pip
 echo "Installing scapy through pip"
-sudo pip install scapy
+pip install scapy
+echo "Installing pypcap"
+pip install pypcap
 echo "You're ready to 0wn, just run `sudo python mail0wner.py`"
