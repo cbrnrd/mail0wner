877a9d5c4466a45e5fff3fb920a065d4de56a751
Carter <cbawsome77@gmail.com>
2016-11-08 11:35:32 -0500

    add -t (--timeout) option

--- mail0wner.py
+++ mail0wner.py
@@ -1,5 +1,11 @@
 from scapy.all import *
 import time
+from optparse import OptionParser
+
+parser = OptionParser(usage="usage: sudo 5prog [options]")
+parser.add_option("-t", "--time", action="store", dest="time", default=100, type=int, help="Total time to sniff packets.")
+(options, args) = parser.parse_args()
+
 
 #packet callback
 def packet_callback(packet):
@@ -10,14 +16,13 @@ def packet_callback(packet):
             print "[*] %s" % packet[TCP].payload
 
 
-print('[*] Running for 100 seconds...')
+print('[*] Running for %i seconds...' % options.time)
 
-    
+
     #start sniffer
-    sniff(filter="tcp port 110 or tcp port 25 or tcp port 143", prn=packet_callback, store=0, timeout=100)
+    sniff(filter="tcp port 110 or tcp port 25 or tcp port 143", prn=packet_callback, store=0, timeout=options.time)
     #port 110 = POP3
     #port 143 = IMAP
     #port 25  = SMTP
-print("[!] Scanner has finished after 100 seconds.")
+print("[!] Scanner has finished after %i seconds." 5 options.time)
 exit(0)
- 
