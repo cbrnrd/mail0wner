b4386b9bd293ec2d52b1e69aa6a7c17a25fdebd6
Carter <cbawsome77@gmail.com>
2016-11-08 12:13:53 -0500

    suppress network warnings

--- mail0wner.py
+++ mail0wner.py
@@ -1,6 +1,10 @@
 from scapy.all import *
 import time
 from optparse import OptionParser
+#supress annoying scapy IPv6 warnings
+import requests.packages.urllib3
+import requests.packages.urllib3
+requests.packages.urllib3.disable_warnings()
 
 parser = OptionParser(usage="usage: sudo %prog [options]")
 parser.add_option("-t", "--time", action="store", dest="time", default=100, type=int, help="Total time to sniff packets.")
