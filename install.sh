72d9a1fd07b7247b36a60c35d6891f411f506961
Carter <cbawsome77@gmail.com>
2016-11-10 18:32:20 -0500

    Switch installs from pip to apt

--- install.sh
+++ install.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
-clear
 if [ "$(id -u)" != "0" ]; then
         echo "Run this script as root!"
         echo "Exiting..."
         exit 1
 fi
-echo "If there are any error messages concerning installs, install whatever they tell you to install \nusing sudo apt-get"
+echo "If there are any error messages concerning installs, install whatever they tell you to install using sudo apt-get"
 
-echo "Installing pip"
-easy_install pip
-echo "Installing scapy through pip"
-pip install scapy
-echo "Installing pypcap"
-pip install pypcap
+
+echo "Installing scapy"
+apt-get install scapy-python
+echo "Installing pypcap and libpcap"
+apt-get install libpcap-dev
+apt-get install python-pypcap
 echo "You're ready to 0wn, just run `sudo python mail0wner.py`"
