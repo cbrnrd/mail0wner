d603a650a8986dc6d07efc6da9c16b077a5b2915
Carter <cbawsome77@gmail.com>
2017-01-18 21:02:47 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -14,11 +14,8 @@ pip install scapy-python
 echo "Installing pypcap and libpcap"
 platform='unknown'
 unamestr=`uname`
-if [ "$unamestr" == 'Linux' ]; then
-   apt-get install libpcap-dev
-elif [ "$unamestr" == 'Darwin']; then
-   pip install pypcap
-fi
+apt-get install libpcap-dev
+pip install pypcap
 pip install netifaces
 echo "You're ready to 0wn."
 chmod +x mail0wner.py
