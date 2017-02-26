4dab695cc29d657aa8e8e6eeda19d788a5b175e2
Carter <cbawsome77@gmail.com>
2017-02-25 19:03:56 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,15 +1,11 @@
 #!/bin/bash
-echo "If there are any error messages concerning installs, install whatever they tell you to install using your package manager"
 
-
-echo "Installing tcpdump"
-apt-get install -y tcpdump
 echo "Installing scapy"
-pip install scapy
+pip install scapy > /dev/null 2>&1
 echo "Installing pypcap and libpcap"
-apt-get install -y libpcap-dev
-pip install pypcap
-pip install netifaces
+apt-get install -y libpcap-dev > /dev/null 2>&1
+brew install libpcap > /dev/null 2>&1
+pip install pypcap > /dev/null 2>&1
+pip install netifaces > /dev/null 2>&1
 echo "You're ready to 0wn."
 chmod +x mail0wner.py
-exit 0
